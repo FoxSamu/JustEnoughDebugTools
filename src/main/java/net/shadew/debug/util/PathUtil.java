bc85fa92704579e175f6c24deea9add7aa19cabc
ShadewRG <onankman@gmail.com>
2021-08-23 22:38:01 +0200

    Yet another attempt to properly locate gametest.json

--- src/main/java/net/shadew/debug/util/PathUtil.java
+++ src/main/java/net/shadew/debug/util/PathUtil.java
@@ -6,7 +6,7 @@ public class PathUtil {
     public static Path resolve(Path root, Path path) {
         if (path.toFile().isAbsolute())
             return path;
-        return root.resolve(path);
+        return root.resolve(path).toAbsolutePath();
     }
 
     public static Path resolve(Path root, String path) {
