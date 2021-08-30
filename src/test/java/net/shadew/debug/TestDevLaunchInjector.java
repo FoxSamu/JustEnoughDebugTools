bf0839ef8b1daeec51fae62a7d0f6d176cf717d3
ShadewRG <onankman@gmail.com>
2021-08-30 20:54:31 +0200

    Test mod is now 'gametest' source set because 'test' is cursed

--- src/test/java/net/shadew/debug/TestDevLaunchInjector.java
+++
@@ -1,9 +0,0 @@
-package net.shadew.debug;
-
-import net.fabricmc.devlaunchinjector.Main;
-
-public class TestDevLaunchInjector {
-    public static void main(String[] args) throws Throwable {
-        Main.main(args);
-    }
-}
